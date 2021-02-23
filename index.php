3426bf087bb16ddda68ee039bf93af7aa1d5d39c
dduartecode <duartesx86@gmail.com>
2021-02-22 22:30:02 -0300

    CRUD categorias

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ use \Slim\Slim;// [C:\e-commerce\vendor\slim\slim\Slim\Slim.php]
 use \Hcode\Page; // [C:\e-commerce\vendor\hcodebr\php-classes\src\Page.php]
 use \Hcode\PageAdmin; // [C:\e-commerce\vendor\hcodebr\php-classes\src\PageAdmin.php]
 use \Hcode\Model\User;// [C:\e-commerce\vendor\hcodebr\php-classes\src\Model\User.php]
-
+use \Hcode\Model\Category;// [C:\e-commerce\vendor\hcodebr\php-classes\src\Model\Category.php]
 $app = new Slim();
 
 // OBS[Com a utilização de rotas, e a automatização dos gat's e set's, o que diferencia de um dado sera inserido, ou retornado é o método. Se for um get, retorna dados, se for post, insere dados].
@@ -231,6 +231,95 @@ $app->post("/administrator/forgot/reset", function(){
 	$page->setTpl("forgot-reset-success");// ("forgot-reset-success") = template
 
 });
+
+$app->get("/administrator/categories", function(){
+
+	User::verifyLogin();
+
+	$categories = Category::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories", [
+		'categories'=>$categories
+	]);
+
+});
+
+$app->get("/administrator/categories/create", function(){
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-create");
+
+});
+
+$app->post("/administrator/categories/create", function(){
+
+	$category = new Category();
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header('Location: /administrator/categories');
+	exit;
+
+});
+
+$app->get("/administrator/categories/:idcategory/delete", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->delete();
+
+	header("Location: /administrator/categories");
+	exit;
+
+});
+
+$app->get("/administrator/categories/:idcategory", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-update", [
+		'category'=>$category->getValues()
+	]);
+
+});
+
+$app->post("/administrator/categories/:idcategory", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header("Location: /administrator/categories");
+	exit;
+
+});
+
+
+
+
 
 /* MÉTODO SLIMFRAMEWORK*/
 $app->run();// run executa todo esse pacote que foi construído dentro da rota, exibindo o conteúdo na tela.

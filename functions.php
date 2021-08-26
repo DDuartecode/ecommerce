96094c7914326eef1524043f28e558d19068d42b
dduartecode <duartesx86@gmail.com>
2021-08-26 12:31:04 -0300

    Meus pedidos e Resumo carrinho

--- functions.php
+++ functions.php
@@ -1,10 +1,11 @@
-<?php 
+<?php
 
+use Hcode\Model\Cart;
 use \Hcode\Model\User;
 
 function formatPrice($vlPrice)
 {
-	if(!$vlPrice > 0) $vlPrice = 0;
+	if (!$vlPrice > 0) $vlPrice = 0;
 
 	return number_format($vlPrice, 2, ",", ".");
 }
@@ -21,4 +22,20 @@ function getUserName()
 	return $user->getdesperson();
 }
 
-?>
+function getCartNrQtd()
+{
+	$cart = Cart::getFromSession();
+
+	$totals = $cart->getProductsTotals();
+
+	return $totals['nrqtd'];
+}
+
+function getCartVlSubTotal()
+{
+	$cart = Cart::getFromSession();
+
+	$totals = $cart->getProductsTotals();
+
+	return formatPrice($totals['vlprice']);
+}
